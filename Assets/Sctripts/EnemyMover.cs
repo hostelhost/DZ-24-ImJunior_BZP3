d9cff5f690aea3a89e26f67f62c6c0a5c7f84512
Илья <Hostel_2008@mail.ru>
2025-02-09 13:46:21 +0300

    Вампиризм.
    осталось откорректировать значения для запуска

--- Assets/Sctripts/EnemyMover.cs
+++ Assets/Sctripts/EnemyMover.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
@@ -10,11 +13,18 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] private float _targetReachMaxDistanse = 0.1f;
 
     private EnemyAnimatorData _animatorData = new EnemyAnimatorData();
+    private Dictionary<Vector2, int> _animations = new Dictionary<Vector2, int>();
     private Vector3 _pathTarget;
     private int _currentIndexOfTarget;
 
     private void Start()
     {
+        _animations.Add(Vector2.zero, _animatorData.Idle);
+        _animations.Add(Vector2.right, _animatorData.RightWalk);
+        _animations.Add(Vector2.left, _animatorData.LeftWalk);
+        _animations.Add(Vector2.up, _animatorData.UpWalk);
+        _animations.Add(Vector2.down, _animatorData.DownWalk);
+
         _currentIndexOfTarget = _path.Length - 1;
         _pathTarget = TakeNextTarget();
     }
@@ -26,14 +36,14 @@ public class EnemyMover : MonoBehaviour
 
     private void Move()
     {
-        Vector3 nextPosition;
+        Vector3 direction;
 
-        if (_detector.GetGlobalTarget() == null)        
-            nextPosition = FollowPath();                  
+        if (_detector.GetGlobalTarget() == null)
+            direction = FollowPath();
         else
-            nextPosition = FollowGlobalTarget();    
+            direction = FollowGlobalTarget();
 
-        ManageAnimator(nextPosition);
+        ManageAnimator(CorrectorDirection(direction));
     }
 
     private Vector2 TakeNextTarget()
@@ -42,10 +52,10 @@ public class EnemyMover : MonoBehaviour
         return _path[_currentIndexOfTarget].transform.position;
     }
 
-    private Vector3 FollowGlobalTarget()
+    private Vector2 FollowGlobalTarget()
     {
         Vector3 nextPosition = Vector3.MoveTowards(transform.position, _detector.GetGlobalTarget().transform.position, _speed * Time.deltaTime);
-        Vector2 direction = (nextPosition - transform.position).normalized;
+        Vector2 direction = (nextPosition - transform.position);
         transform.position = nextPosition;
         return direction;
     }
@@ -53,7 +63,7 @@ public class EnemyMover : MonoBehaviour
     private Vector2 FollowPath()
     {
         Vector3 nextPosition = Vector3.MoveTowards(transform.position, _pathTarget, _speed * Time.deltaTime);
-        Vector2 direction = (nextPosition - transform.position).normalized;
+        Vector2 direction = (nextPosition - transform.position);
         transform.position = nextPosition;
 
         if (Vector2.SqrMagnitude(transform.position - _pathTarget) <= _targetReachMaxDistanse * _targetReachMaxDistanse)
@@ -64,16 +74,21 @@ public class EnemyMover : MonoBehaviour
 
     private void ManageAnimator(Vector2 direction)
     {
-        Dictionary<Vector2, int> animations = new Dictionary<Vector2, int>();
+        if (_animations.TryGetValue(direction, out int value))
+            _animator.Play(value);
+    }
 
-        animations.Add(Vector2.zero, _animatorData.Idle);
-        animations.Add(Vector2.right, _animatorData.RightWalk);
-        animations.Add(Vector2.left, _animatorData.LeftWalk);
-        animations.Add(Vector2.up, _animatorData.UpWalk);
-        animations.Add(Vector2.down, _animatorData.DownWalk);
+    private Vector2 CorrectorDirection(Vector2 direction)
+    {
+        float coordinateX = direction.x;
+        float coordinateY = direction.y;
+        Math.Round(coordinateX);
+        Math.Round(coordinateY);
 
+        direction = new Vector2(coordinateX, coordinateY); 
 
-       
+        // œ–Œ¬≈–»“‹ ƒ≈¡¿√ŒÃ ◊“Œ Õ≈ “¿ ! œÓ‚ÂËÚ¸ ¬  ¿ ”ﬁ —“Œ–ŒÕ”  Œ––≈ “»–”≈“—ﬂ! !!! !
+        return direction;
     }
 }
 
